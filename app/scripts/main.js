85141f56ece04a14e5f126f976c135e3497413c0
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-04-29 14:44:43 -0400

    Remove yeoman given code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,45 +1,45 @@
 /*global Galorie, $*/
 
 
-window.Galorie = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
-};
+// window.Galorie = {
+//     Models: {},
+//     Collections: {},
+//     Views: {},
+//     Routers: {},
+//     init: function () {
+//         'use strict';
+//         console.log('Hello from Backbone!');
+//     }
+// };
 
-$(document).ready(function () {
-    'use strict';
-    Galorie.init();
-});
+// $(document).ready(function () {
+//     'use strict';
+//     Galorie.init();
+// });
 
 //Backbone Model
-var Calorie = Backbone.Model.extend({
+var Galorie = Backbone.Model.extend({
     defaults: {
         food : '',
         portion: '',
-        calorie_intake: ''
+        galorie_intake: ''
     }
 });
 
 //Backbone Collection
-var Calories = Backbone.Collection.extend({})
+var Galories = Backbone.Collection.extend({})
 
-var breakfast = new Calorie({food:'banana', portion:3, calorie_intake:10 });
-var lunch = new Calorie({food:'sandwich', portion:1, calorie_intake:400 });
-var dinner = new Calorie({food:'veggie_burger', portion:1, calorie_intake:250 }); 
+var breakfast = new Galorie({food:'banana', portion:3, galorie_intake:10 });
+var lunch = new Galorie({food:'sandwich', portion:1, galorie_intake:400 });
+var dinner = new Galorie({food:'veggie_burger', portion:1, galorie_intake:250 }); 
     
 //instantiate Collection
-var calories = new Calorie();
+var galories = new Galorie();
 
 //Backbone View for one calorie
-var CalorieView = Backbone.View.extend({
-    model: new Calorie(),
-    tagName: ‘tr’,
+var GalorieView = Backbone.View.extend({
+    model: new Galorie(),
+    tagName: 'tr',
     initialize: function(){
       this.template = _.template($('.calories-list-template').html())         
     },
@@ -49,5 +49,37 @@ var CalorieView = Backbone.View.extend({
     }
 });
 
+//Backbone View for all calories
+var GaloriesView = Backbone.View.extend({
+  model: galories,
+  el: $('.calories-list'),
+  initialize: function(){
+    this.model.on('add', this.render,this);
+  },
+  render: function(){
+    var self = this; 
+    this.$el.html('');
+    _.each(this.model.toArray(), function(galorie){
+    // iterate through each blog in our blogs array
+      self.$el.append((new GalorieView({model: galorie})).render().$el);
+    });
+    return this;
+
+  }
+});
+
+var galoriesView = new GaloriesView();
+
+$(document).ready(function(){
+  $('.add-blog').on('click', function(){
+    var calorie = new Galorie({
+      food: $('.food-input').val(),
+      portion: $('.portion-input').val(),
+      galorie_intake: $('.calorie_intake-input').val()      
+    });
+      console.log(galorie.toJSON());
+      galories.add(galorie);
+  });
+});  
 
 
